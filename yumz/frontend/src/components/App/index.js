33c29c88afb0e3f9982349abb49db5b3a79146e7
notjoyu <notjoyu@users.noreply.github.com>
2021-01-30 15:49:24 -0800

    Added app and landing page

---
+++ yumz/frontend/src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from '../Landing';
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <LandingPage />
+        </React.Fragment>
+        )
+}
+
+export default App;
---
+++ yumz/frontend/src/components/Landing/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const LandingPage = (props) => {
+  const [buttonClicked, setButtonClicked] = useState(true);
+
+  return (
+    <React.Fragment>
+      This is the home page.
+    </React.Fragment>
+  )
+}
+
+
+export default LandingPage;
