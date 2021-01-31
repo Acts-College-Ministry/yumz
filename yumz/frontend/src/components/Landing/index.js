93fd6992fb8dd1fa885a4b944a01c16ae7724706
Corey Tan <coreyct@uci.edu>
2021-01-30 20:21:11 -0800

    landing changes

--- yumz/frontend/src/components/Landing/index.js
+++ yumz/frontend/src/components/Landing/index.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'
 
-const LandingPage = (props) => {
-  	const [buttonClicked, setButtonClicked] = useState(true);
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
+import { Grid, Button, ButtonBase } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
-	render() {
-		return (
-			<header id="header">
-				<div className="intro">
-					<div className="overlay">
-						<div className="container">
-							<div className="row">
-								<div className="col-md-8 col-md-offset-2 intro-text">
-									<h1> hola
+import * as ROUTES from '../../constants/routes';
 
-									</h1>
-									<a href="/home"> </a>
-		);
-	}
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  cardAction: {
+    display: 'block',
+    textAlign: 'initial'
+  },
+  title: {
+    fontSize: 14,
+  },
+  media: {
+  	height: 20,
+    width: 20,
+    objectFit: 'cover',
+  },
+  pos: {
+  	marginTop: 12,
+    marginBottom: 12,
+  },
+  nopad: {
+  	padding: 0,
+    "&:last-child": {
+      paddingBottom: 0
+    }
+  },
+});
+
+export default function LandingPage() {
+	const classes = makeStyles();
+	return(
+		<Grid container direction="column" alignItems="center" justify="center" style={{ minHeight: '100vh' }}>
+			<Card className={classes.root}>
+				<CardContent>
+			    	<img className={classes.media} src="img/yumz.jpg"></img>
+			    </CardContent>
+			</Card>
+			<Link underline='none' to={'/home'} style={{ textDecoration: 'none' }}>
+				<Card className={classes.root}>
+			    	<CardContent className={classes.nopad}>
+			        	<Typography variant="h1">
+			          	HUNGRY ?
+			        	</Typography>
+			      	</CardContent>
+			    </Card>
+		    </Link>
+		</Grid>
+	);
 }
 
-export default LandingPage;
