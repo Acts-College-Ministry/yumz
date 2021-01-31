756a4a29bdbd4a021d780d102d1013ec96df5874
notjoyu <notjoyu@users.noreply.github.com>
2021-01-30 16:41:33 -0800

    Update index.js

--- yumz/frontend/src/components/App/index.js
+++ yumz/frontend/src/components/App/index.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 
-import { makeStyles } from '@material-ui/core'
-
 import LandingPage from '../Landing';
 import HomePage from '../Home';
 import AboutPage from '../About';
 
 import * as ROUTES from '../../constants/routes';
 
-const useStyles = makeStyles((theme) => ({
-
-}))
-
 const App = () => {
-    const classes = useStyles();
-
     return (
         <React.Fragment>
             <Router>
-                <div>
-                    <Switch>
-                        <Route exact path={ROUTES.LANDING} component={LandingPage} />
-                        <Route path={ROUTES.HOME} component={HomePage} />
-                        <Route path={ROUTES.ABOUT} component={AboutPage} />
-                        <Redirect from='/' to={ROUTES.LANDING} />
-                    </Switch>
-                </div>
+                <Switch>
+                    <Route exact path={ROUTES.LANDING} component={LandingPage} />
+                    <Route path={ROUTES.HOME} component={HomePage} />
+                    <Route path={ROUTES.ABOUT} component={AboutPage} />
+                    <Redirect from='/' to={ROUTES.LANDING} />
+                </Switch>
             </Router>
         </React.Fragment>
         )
