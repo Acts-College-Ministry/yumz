0497074d7233ba0a79318e84b90d1be889c3419c
notjoyu <notjoyu@users.noreply.github.com>
2021-01-30 21:31:22 -0800

    added back button to aobut page

--- yumz/frontend/src/components/About/index.js
+++ yumz/frontend/src/components/About/index.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { Button, TextField } from '@material-ui/core';
+
+import * as ROUTES from '../../constants/routes';
 
 const AboutPage = (props) => {
   const [buttonClicked, setButtonClicked] = useState(true);
 
+  const onClick = (event) => {
+    props.history.push(ROUTES.HOME);
+    event.preventDefault();
+  }
+
   return (
     <React.Fragment>
       This is the about page.
+      <Button
+        variant="outlined"
+        color="primary"
+        type="submit"
+        onClick={onClick}
+      >
+        Back
+      </Button>
     </React.Fragment>
   )
 }
--- yumz/frontend/src/components/Home/recommendation.js
+++ yumz/frontend/src/components/Home/recommendation.js
@@ -2,10 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { Button, TextField } from '@material-ui/core';
 
 const RecommendationBase = (props) => {
+    const onClick = (event) => {
+        props.setLocation('');
+        props.setSubmit(false);
+        props.setRecommendation(false);
+        event.preventDefault();
+    }
     return (
         <React.Fragment>
             Here's your recommendation.
             Thanks for swiping, come again!
+            <Button
+              variant="outlined"
+              color="primary"
+              type="submit"
+              onClick={onClick}
+              //className={classes.submitButton}
+            >
+            Try Again!
+            </Button>
         </React.Fragment>
     )
 }
