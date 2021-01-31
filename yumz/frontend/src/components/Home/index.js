63366c6073ce09455a02e9fc12360d0ac97346ce
notjoyu <notjoyu@users.noreply.github.com>
2021-01-30 20:57:22 -0800

    swipe/location/recommendation swapping

--- yumz/frontend/src/components/Home/index.js
+++ yumz/frontend/src/components/Home/index.js
@@ -2,18 +2,52 @@ import React, { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
 
 import Location from './location';
+import Swipe from './swipe';
+import Recommendation from './recommendation';
 
 import * as ROUTES from '../../constants/routes';
 
 const HomePage = (props) => {
-  //const [buttonClicked, setButtonClicked] = useState(true);
+  const [location, setLocation] = useState('');
+  const [submit, setSubmit] = useState(false);
+  const [recommendation, setRecommendation] = useState(false);
+
   const onClick = (event) => {
     props.history.push(ROUTES.ABOUT);
     event.preventDefault();
   }
   return (
     <React.Fragment>
-      <Location />
+
+
+      {
+        (location === '')
+        &&
+        (!submit)
+        &&
+        (!recommendation)
+        &&
+        <Location
+          setLocation={setLocation}
+          setSubmit={setSubmit}
+        />
+      }
+      {
+        (submit)
+        &&
+        (!recommendation)
+        &&
+        <Swipe
+          setRecommendation={setRecommendation}
+        />
+      }
+      {
+        (submit)
+        &&
+        (recommendation)
+        &&
+        <Recommendation />
+      }
       <Button
         variant="outlined"
         color="primary"
